eea46037f666f7f1ea56c7b6b43a33429a67d8bc
ddkddown <2628397115@qq.com>
2020-12-23 22:27:55 +0800

    [update] 完善socket的RAII获取方式

--- bases/Socket.h
+++ bases/Socket.h
@@ -7,6 +7,7 @@ extern "C" {
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <assert.h>
 }
 
 namespace network {
@@ -20,10 +21,31 @@ public:
 IgnoreSig initSig;
 
 class SocketWrapper {
-//TODO RAII tcpServer
-private:
-struct sockaddr_in myAddr_;
-int sockFd;
+public:
+    SocketWrapper(int port) {
+        sockFd_ = socket(AF_INET, SOCK_STREAM, 0);
+        myAddr_.sin_family = AF_INET;
+        myAddr_.sin_port = htons(port);
+        myAddr_.sin_addr.s_addr = htonl(INADDR_ANY);
+    }
 
+    void Bind() {
+        assert(0 == bind(sockFd_, (struct sockaddr*)&myAddr_,
+                                         sizeof myAddr_));
+    }
+
+    void Listen(int size) {
+        listen(sockFd_, size);
+    }
+
+    int Accept() {
+        socklen_t peerLen = sizeof peerAddr_;
+        return accept(sockFd_, (struct sockaddr*)&peerAddr_, &peerLen);
+    }
+    
+private:
+    struct sockaddr_in myAddr_;
+    struct sockaddr_in peerAddr_;
+    int sockFd_;
 };
 };
