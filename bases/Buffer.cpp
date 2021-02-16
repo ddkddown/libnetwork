9bacc1cb4af3860eed0368a55e2992c33482420b
ddkddown <2628397115@qq.com>
2021-02-16 17:40:39 +0800

    [update] 创建应用层缓存buff

---
+++ bases/Buffer.cpp
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include "Buffer.h"
+Buffer::Buffer():buff_(INITIAL_SIZE) {}
+
+Buffer::~Buffer(){}
+
+int Buffer::ReadFromFd(int fd) {
+    char tmp[65536] = {0};
+    int n = recv(fd, tmp, 65536, 0);
+    if(n > 0) {
+        if((buff_.capacity() - buff_.size()) < n) {
+            buff_.resize(buff_.size() + n);
+        }
+
+        memcpy(&*(buff_.begin()+buff_.size()), tmp, n);
+    }
+
+    return n;
+}
+
+void Buffer::SendToFd(int fd) {
+    if(buff_.empty()) {
+        return;
+    }
+
+    send(fd, &*buff_.begin(), buff_.size(), 0);
+}
---
+++ bases/Buffer.h
@@ -0,0 +1,21 @@
+//减少recv，send调用，尽量读取数据，防止send阻塞
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Buffer {
+enum {
+    INITIAL_SIZE = 1024
+};
+public:
+    Buffer();
+    ~Buffer();
+    int ReadFromFd(int fd);
+    void SendToFd(int fd);
+    void AppenData(const char *data, int len);
+
+private:
+    vector<char> buff_;
+};
